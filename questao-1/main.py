ddd35207d6eba013c3d1cd26952ee76449394fe2
caio nogueira <cnog96@gmail.com>
2026-07-01 21:14:43 -0300

    segunda parte do nosso codigo

--- questao-1/main.py
+++ questao-1/main.py
@@ -1,6 +1,13 @@
 def calcular_valor_total(listacompras):
-    total = 0
+    total = 0.0
     for nome, categoria, valor in listacompras:
         total += valor
-        pass
     return total
+
+def calcular_valor_frutas_verduras(listacompras):
+    total = 0.0
+    for nome, categoria, valor in listacompras:
+        if categoria == "frutas" or categoria == "verduras":
+            total += valor
+    return total
+    
