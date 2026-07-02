71495007fb769362e77d103e78714ac7afbcb901
caio nogueira <cnog96@gmail.com>
2026-07-01 21:27:59 -0300

    terceira parte do codigo

--- questao-1/main.py
+++ questao-1/main.py
@@ -10,4 +10,20 @@ def calcular_valor_frutas_verduras(listacompras):
         if categoria == "frutas" or categoria == "verduras":
             total += valor
     return total
+
+def calcular_desconto(valor_frutas_verduras, eh_fidelidade, idade):
+    desconto_total = 0.0
     
+    if idade > 60:
+        desconto_total += 0.10
+        
+    if eh_fidelidade == True:
+        desconto_total += 0.05
+        
+    if valor_frutas_verduras > 30.00:
+        desconto_total += 0.05
+    
+    if desconto_total > 0.20:
+        desconto_total = 0.20
+    
+    return desconto_total
